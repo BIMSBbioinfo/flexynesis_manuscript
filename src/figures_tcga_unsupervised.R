3afb09dfaf3408543b65c9a23db820686e42b7d4
Bora Uyar <bora.uyar@mdc-berlin.de>
2024-05-31 09:45:33 +0200

    add code and figures for making the figure for unsupervised clustering of tcga samples

---
+++ src/figures_tcga_unsupervised.R
@@ -0,0 +1,47 @@
+# figures for unsupervised clustering of tcga samples 
+library(data.table)
+library(ggplot2)
+library(ggpubr)
+library(ggrepel)
+ggplot2::theme_set(ggpubr::theme_pubclean())
+
+source('/fast/AG_Akalin/buyar/flexynesis_manuscript_work/flexynesis_manuscript/src/utils.R')
+
+folder <- '/fast/AG_Akalin/buyar/flexynesis_manuscript_work/analyses/unsupervised_cancertype/'
+
+get_plot <- function(df_tsne, factors, label_size = 5) {
+  centroids <- data.frame(get_basis_matrix(df_tsne, factors))
+  colnames(centroids) <- c('x', 'y')
+  df_tsne$factor <- factors
+  centroids$group <- as.factor(rownames(centroids))
+  p <- ggplot(df_tsne, aes(x = tSNE1, y = tSNE2)) +
+    geom_point(aes(color = factor), alpha = 0.6) +
+    geom_text_repel(data = centroids, aes(x = x, y = y, 
+                                    label = group), 
+              size = label_size) + 
+  theme(legend.position = 'none', text = element_text(size = 18)) 
+  return(p)
+}
+
+E <- read.csv(file.path(folder, 'cancertype.embeddings_train.csv'), row.names = 1)
+df_tsne <- plot_tsne(E, returnData = T)
+
+clusters <- read.csv(file.path(folder, 'clusters.csv'), header = T, row.names = 1)
+rownames(clusters) <- clusters$sample
+clusters$cohort <- gsub("TCGA-", "", clusters$label)
+
+p1 <- get_plot(df_tsne, as.factor(clusters[rownames(df_tsne), 'cluster']), label_size = 5)
+p1 
+p2 <- get_plot(df_tsne, as.factor(clusters[rownames(df_tsne), 'cohort']), label_size = 5)
+p2
+p3 <- plot_cluster_comparison(clusters[,'cluster',drop=F], clusters[,'cohort',drop=F])
+p3
+
+p <- cowplot::plot_grid(cowplot::plot_grid(p1, p2, ncol = 1, labels = c('A', 'B')), 
+                   p3, nrow = 1, rel_widths = c(1.5, 1), labels = c('A', 'C'))
+
+
+
+
+
+
---
+++ src/prepare_data.tcga_cancertype.R
@@ -0,0 +1,66 @@
+# prepare data for unsupervised clustering experiment for tcga 
+library(pbapply)
+source('./src/utils.R')
+
+dataDir <- '/data/local/buyar/arcas/pancancer_multiomics_manuscript/tcga/data/GDCdata'
+
+projects <- unlist(strsplit('TCGA-BLCA,TCGA-BRCA,TCGA-CESC,TCGA-COAD,TCGA-ESCA,TCGA-HNSC,TCGA-KIRC,TCGA-KIRP,TCGA-LGG,TCGA-LIHC,TCGA-LUAD,TCGA-LUSC,TCGA-PAAD,TCGA-PCPG,TCGA-PRAD,TCGA-SARC,TCGA-STAD,TCGA-TGCT,TCGA-THCA,TCGA-UCEC', 
+                     ','))
+
+# read gex+meth for each project
+
+combine_matrices <- function(l) {
+  common_features <- Reduce(intersect, lapply(l, rownames))
+  if(length(common_features) > 100000) {
+    common_features <- sample(common_features, 100000)
+  }
+  combined <- do.call(cbind, lapply(l, function(x) {
+    x[common_features,]
+  }))
+  labels <- do.call(c, lapply(names(l), function(x) {
+    rep(x, ncol(l[[x]]))
+  }))
+  return(list('dat' = combined, 'labels' = labels))
+}
+
+import_dat <- function(dataDir, projects, suffix, nodes = 3) {
+  cl <- parallel::makeCluster(nodes)
+  parallel::clusterExport(cl = cl, varlist = c('dataDir', 'projects'))
+  dat <- pbapply::pbsapply(simplify = F, cl = cl, projects, function(x) {
+    obj <- readRDS(file.path(dataDir, paste0(x, suffix)))
+    M <- SummarizedExperiment::assay(obj)
+    # update colnames to keep patient barcodes (not sample barcodes)
+    colnames(M) <- sub("(^TCGA.{8}).+$", "\\1", colnames(M))
+    return(M)
+  })
+  parallel::stopCluster(cl)
+  return(dat)
+}
+
+gex <- import_dat(dataDir, projects, ".gex.fpkm.RDS", nodes = 10)
+meth <- import_dat(dataDir, projects, ".meth.RDS", nodes = 10)
+
+# find common samples in both and downsample 
+for (x in projects) {
+  s <- intersect(colnames(gex[[x]]), colnames(meth[[x]]))
+  # downsample 
+  s <- sample(s, 100)
+  gex[[x]] <- gex[[x]][,s]
+  meth[[x]] <- meth[[x]][,s]
+}
+
+gexC <- combine_matrices(gex)
+methC <- combine_matrices(meth)
+
+samples <- intersect(colnames(gexC$dat), colnames(methC$dat))
+
+dat <- list('gex' = gexC$dat[,samples], 'meth' = methC$dat[,samples], 
+            'clin' = data.frame('cohort' = gexC$labels[match(samples, colnames(gexC$dat))], 
+                                row.names = samples))
+
+dat_split <- split_dat(dat, ratio = 0.8)
+
+print_dataset(dat_split, outdir = '../data/tcga_cancertype')
+
+
+
